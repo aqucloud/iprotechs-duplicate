289d2ba83b2fdb0aa3bcea1fab79b0ad29dea40e
viniladapa <vinil.adapa@iprotechs.com>
2025-10-01 09:07:44 +0530

    Removed honeypot, js_challenges related validation logic and updating raw message by adding MIME version and content type text/html to email clients (Gmail, Outlook, etc.) will render HTML markup as styled email instead of printing raw tags.

--- backend/src/email.ts
+++ backend/src/email.ts
@@ -23,62 +23,15 @@ export const contact: ExportedHandlerFetchHandler<Env> = async (request, environ
 			});
 		}
 		
-		// Extract the reCAPTCHA token and client IP
-		// const captcha = formData["g-recaptcha-response"] || "";
+		// Extracting the client IP
 		const ip = request.headers.get("cf-connecting-ip") || "unknown";
-
-		// Ensure a token was provided
-		// if (!captcha) {
-		// 	return new Response(JSON.stringify({
-		// 		success: false,
-		// 		message: "No reCAPTCHA token provided"
-		// 	}), {
-		// 		status: 400,
-		// 		headers: { "Content-Type": "application/json" }
-		// 	});
-		// }
-
-		// try {
-		// 	// Verify the token with Google
-		// 	const captchaRes = await fetch("https://www.google.com/recaptcha/api/siteverify", {
-		// 		method: "POST",
-		// 		headers: { "Content-Type": "application/x-www-form-urlencoded" },
-		// 		body: new URLSearchParams({
-		// 			secret: '6Le8E-QqAAAAADIgywfXI_0WBUBumNy0KaBqSTFG',
-		// 			response: captcha,
-		// 			remoteip: ip
-		// 		})
-		// 	});
-
-		// 	// Parse the JSON response
-		// 	const captchaData: RecaptchaResponse = await captchaRes.json();
-
-		// 	if (!captchaData.success) {
-		// 		return new Response(JSON.stringify({
-		// 			success: false,
-		// 			message: "reCAPTCHA validation failed"
-		// 		}), {
-		// 			status: 400,
-		// 			headers: { "Content-Type": "application/json" }
-		// 		});
-		// 	}
-		// } catch (err) {
-		// 	console.error("reCAPTCHA verification error:", err);
-		// 	return new Response(JSON.stringify({
-		// 		success: false,
-		// 		message: "Error verifying reCAPTCHA"
-		// 	}), {
-		// 		status: 500,
-		// 		headers: { "Content-Type": "application/json" }
-		// 	});
-		// }
 
 		// Prepare email bodies
 		const userEmailBody = getUserEmailBody(formData);
-		const adminEmailBody = getAdminEmailBody(formData);
+		const adminEmailBody = getAdminEmailBody(formData, ip);
 
 		// Send emails using your existing sendEmail function
-		// await sendEmail(environment, "info@iprotechs.com", formData.email, "Thank you for your Demo Request", userEmailBody);
+		await sendEmail(environment, "info@iprotechs.com", formData.email, "Thank you for your Demo Request", userEmailBody);
 		await sendEmail(environment, formData.email, "info@iprotechs.com", `You have a demo request from ${formData.name}!`, adminEmailBody);
 
 		return new Response(JSON.stringify({ success: true, message: "Request submitted successfully" }), {
@@ -92,17 +45,23 @@ export const contact: ExportedHandlerFetchHandler<Env> = async (request, environ
 	}
 };
 
-// Define type for Google reCAPTCHA response
-interface RecaptchaResponse {
-  success: boolean;
-}
-
 //////////////////////////////
 //////// Email Helper
 //////////////////////////////
 const sendEmail = async (environment: Env, fromEmail: string, toEmail: string, subject: string, body: string) => {
 	// RAW RFC 5322 Message 📧;
-	const rawMessage = [`From: <${fromEmail}>`, `To: ${toEmail}`, `Reply-To: ${fromEmail}`, `Date: ${new Date().toUTCString()}`, `Message-ID: <${crypto.randomUUID()}@iprotechs.com>`, `Subject: ${subject}`, '', body].join('\n');
+	const rawMessage = [
+		`From: <${fromEmail}>`, 
+		`To: ${toEmail}`, 
+		`Reply-To: ${fromEmail}`, 
+		`Date: ${new Date().toUTCString()}`, 
+		`Message-ID: <${crypto.randomUUID()}@iprotechs.com>`, 
+		`Subject: ${subject}`, 
+		`MIME-Version: 1.0`,
+    	`Content-Type: text/html; charset=UTF-8`,
+		``, 
+		body
+	].join('\n');
 	const emailMessage = new EmailMessage(fromEmail, toEmail, rawMessage);
 	await environment.SEB.send(emailMessage);
 };
@@ -147,7 +106,7 @@ const getUserEmailBody = (formData: Record<string, string>) => {
 				</html>`;
 }
 
-const getAdminEmailBody = (formData: Record<string, string>) => {
+const getAdminEmailBody = (formData: Record<string, string>, clientIp: string) => {
    return `<!DOCTYPE html>
 				<html>
 					<body>
@@ -174,7 +133,7 @@ const getAdminEmailBody = (formData: Record<string, string>) => {
 																	<tr><td>Mobile</td><td>${formData.phone}</td></tr>
 																	<tr><td>Subject</td><td>${formData.subject}</td></tr>
 																	<tr><td>Message</td><td>${formData.message}</td></tr>
-																	<tr><td>IP Address</td><td>${formData.ip}</td></tr>
+																	<tr><td>IP Address</td><td>${clientIp}</td></tr>
 																</tbody>
 															</table>
 														</td>
